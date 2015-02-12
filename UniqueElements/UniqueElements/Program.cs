d166f4918107b5f247d8b3260589c3e800df96d8
darkhelios <tomas.ibarrab@gmail.com>
2015-02-11 21:32:31 -0700

    Code-Eval
    Practices from code-eval

---
+++ UniqueElements/UniqueElements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arreglo = "1,1,1,2,2,3,3,4,4".Split(',');
+
+            var arregloFinal = arreglo.Distinct().ToArray();
+
+            Console.WriteLine(string.Join(",", arregloFinal));
+            Console.ReadKey();
+
+        }
+    }
+}   
