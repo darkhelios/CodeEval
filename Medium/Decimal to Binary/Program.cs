eb10743a4bb9f95d5277d69b8adc083e06697494
darkhelios <tomas.ibarrab@gmail.com>
2015-02-21 01:11:03 -0700

    Code-Eval
    Code-Eval

---
+++ Medium/Decimal to Binary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText("C:\\Users\\Tomas Alfonso\\Desktop\\texto.txt"))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+            int num = Int32.Parse(line);
+
+            string binario = Convert.ToString(num, 2);
+            Console.WriteLine(binario);
+
+        }
+    }
+}
---
+++ Medium/Numbers of Ones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText("C:\\Users\\Tomas Alfonso\\Desktop\\texto.txt"))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                Debug.WriteLine(NumBits(Int32.Parse(line)));
+
+            }
+    }
+
+    static int NumBits(int n)
+    {
+        int cont = 0;
+        while (n != 0)
+        {
+            cont++;
+            n &= (n - 1);
+        }
+        return cont;
+    }
+
+}
