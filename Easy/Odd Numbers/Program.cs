c3290bf4eec50b8d2b494736e10443938e6416dc
darkhelios <tomas.ibarrab@gmail.com>
2015-02-21 15:20:05 -0700

    Code-Eval
    Code-Eval

---
+++ Easy/Odd Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+
+    }
+}
