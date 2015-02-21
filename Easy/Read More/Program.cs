16d8d25ac707473e6d2685d9cc5a4c35e4a82e00
darkhelios <tomas.ibarrab@gmail.com>
2015-02-21 03:14:47 -0700

    Code-Eval
    Code-Eval

---
+++ Easy/Read More/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                var letras = line.ToArray();
+
+                if (letras.Length <= 55)
+                {
+                    Console.WriteLine(string.Join("", letras));
+                    
+                }
+                else
+                {
+                    string read = "... <Read More>";
+
+                    string str = line.Substring(0, 40);
+                    
+                    if(str.Contains(" ")){
+                        
+                        str = line.Substring(0, str.LastIndexOf(' ')).Trim();
+
+                    }
+
+                    Console.WriteLine(str + read);
+                }
+            }
+    }
+}
