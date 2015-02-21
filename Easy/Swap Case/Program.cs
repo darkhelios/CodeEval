177289cbe25f8bcd89c84b79935d118b05311ce8
darkhelios <tomas.ibarrab@gmail.com>
2015-02-20 21:11:50 -0700

    Code-Eval
    Code-Eval

---
+++ Easy/Swap Case/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                char[] c = line.ToCharArray();
+
+
+
+                foreach (char caracter in c)
+                {
+                    if (char.IsLower(caracter))
+                    {
+                        Console.Write(char.ToUpper(caracter));
+
+                    }
+                    else
+                    {
+                        Console.Write(char.ToLower(caracter));
+                    }
+
+                }
+
+                Console.WriteLine("");
+
+            }
+    }
+}
---
+++ Medium/ArrayAbsurdity/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                string[] letras = line.Split(';');
+                string[] a = letras[1].Split(',');
+                string[] b = a.Distinct().ToArray();
+
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    for (int j = i + 1; j < a.Length; j++)
+                    {
+                        if (a[i] == a[j])
+                        {
+                            Console.WriteLine(a[j]);
+                        }
+                    }
+
+                }
+            }
+    }
+}
