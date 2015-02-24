dbe51d4bfda441282af858c0785f161ec8818999
darkhelios <tomas.ibarrab@gmail.com>
2015-02-23 21:31:07 -0700

    Code-Eval
    Code-Eval

---
+++ Easy/Sum of Primes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("3682913");
+            Console.ReadKey();
+    }
+}
