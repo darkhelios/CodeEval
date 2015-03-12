6bbbf3a61c2ba58e64becfa07b5d6dc7e1569225
Tomas Ibarra <tomas.ibarrab@gmail.com>
2015-03-12 10:18:43 -0700

    Coding_Wednesday
    Remove Characters!

---
+++ Moderate/Remove Characters/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string c = "";
+
+        using (StreamReader reader = File.OpenText("C:\\Users\\CsiPro\\Desktop\\texto.txt"))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                String[] word = line.Split(',');
+                String a = word[0].Trim();
+                String b = word[1].Trim();
+                
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    for (int j = 0; j < b.Length; j++)
+                    {
+                        if(a[i]==b[j]){
+
+                            if(c==""){
+                                c = a.Replace(a[i].ToString(), "");
+                            }
+                            else
+                            {
+                                c=c.Replace(a[i].ToString(), "");
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(c);
+                Console.ReadKey();
+                c = "";
+            }
+    }
+}
