ad2dd99244b408742c09d0760952cc192dfd5e71
Tomas Ibarra <tomas.ibarrab@gmail.com>
2015-03-12 11:39:11 -0700

    Easy gg
    Pentiultimate Word

---
+++ Easy/Pentiultimate Word/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText("C:\\Users\\CsiPro\\Desktop\\texto.txt"))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+            String[] words = line.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if(i==words.Length-2){
+                    Console.WriteLine(words[i]);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
